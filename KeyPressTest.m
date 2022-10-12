95f26eac45d76382ed7487a4404ef325a3642ae4
rlhon08 <107014628+rlhon08@users.noreply.github.com>
2022-10-12 13:46:27 -0700

    Turns Added

--- KeyPressTest.m
+++ KeyPressTest.m
@@ -5,15 +5,17 @@ while 1
     pause(0.1);
     switch key
         case 'uparrow'
-            brick.MoveMotor('A',50);
-            brick.MoveMotor('D',50);
+            brick.MoveMotor('A',100);
+            brick.MoveMotor('D',100);
         case 'downarrow'
-            brick.MoveMotor('A',-50);
-            brick.MoveMotor('D',-50);
-        case 'leftarrow'
-            disp('Left Arrow Pressed!');
+            brick.MoveMotor('A',-100);
+            brick.MoveMotor('D',-100);
         case 'rightarrow'
-            disp('Right Arrow Pressed');
+            brick.MoveMotor('A',50);
+            brick.MoveMotor('D',-30);
+        case 'leftarrow'
+            brick.MoveMotor('D',50);
+            brick.MoveMotor('A',-30);
         case 0
             brick.StopAllMotors;
         case 'q'
