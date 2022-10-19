efce8b54938903757f91bf0acebb2ab868ec75c7
rlhon <107014628+rlhon@users.noreply.github.com>
2022-10-19 12:58:36 -0700

    Added comments to the remote control

--- KeyPressTest.m
+++ KeyPressTest.m
@@ -4,25 +4,25 @@ InitKeyboard();
 while 1
     pause(0.1);
     switch key
-        case 'downarrow'
+        case 'downarrow' % down arrow moves it backwards
             brick.MoveMotor('A',90);
             brick.MoveMotor('B',90);
-        case 'uparrow'
+        case 'uparrow' % up arrow moves it forward
             brick.MoveMotor('A',-90);
             brick.MoveMotor('B',-90);
-        case 'rightarrow'
+        case 'rightarrow' % right arrow turns it right
             brick.MoveMotor('A',40);
             brick.MoveMotor('B',-20);
-        case 'leftarrow'
+        case 'leftarrow' % left arrow turns it left
             brick.MoveMotor('B',40);
             brick.MoveMotor('A',-20);
-        case 'w'
+        case 'w' %opens the claw
             brick.MoveMotor('D',30);
-        case 's'
+        case 's'%closes the claw
             brick.MoveMotor('D',-30);
-        case 0
+        case 0 % stops motors when no keys are pressed
             brick.StopAllMotors;
-        case 'q'
+        case 'q' %q breaks the entire program
             break;
     end
 end
