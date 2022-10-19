1cf5ff4c00aaf72f1f793f65e1cf63a011e548e1
rlhon <107014628+rlhon@users.noreply.github.com>
2022-10-19 12:35:08 -0700

    The full remote control

--- KeyPressTest.m
+++ KeyPressTest.m
@@ -4,18 +4,22 @@ InitKeyboard();
 while 1
     pause(0.1);
     switch key
-        case 'uparrow'
-            brick.MoveMotor('A',100);
-            brick.MoveMotor('D',100);
         case 'downarrow'
-            brick.MoveMotor('A',-100);
-            brick.MoveMotor('D',-100);
+            brick.MoveMotor('A',90);
+            brick.MoveMotor('B',90);
+        case 'uparrow'
+            brick.MoveMotor('A',-90);
+            brick.MoveMotor('B',-90);
         case 'rightarrow'
-            brick.MoveMotor('A',50);
-            brick.MoveMotor('D',-30);
+            brick.MoveMotor('A',40);
+            brick.MoveMotor('B',-20);
         case 'leftarrow'
-            brick.MoveMotor('D',50);
-            brick.MoveMotor('A',-30);
+            brick.MoveMotor('B',40);
+            brick.MoveMotor('A',-20);
+        case 'w'
+            brick.MoveMotor('D',30);
+        case 's'
+            brick.MoveMotor('D',-30);
         case 0
             brick.StopAllMotors;
         case 'q'
